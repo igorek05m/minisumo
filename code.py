03eede7b32732a3ed3a8dc9c38140e11bb4932f0
Igiii <43452773+igorek05m@users.noreply.github.com>
2024-04-13 22:00:36 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,52 +1,148 @@
 from sumolib import *
 import time
 
-print('hej')
-
-lewySilnik, prawySilnik= motors_init()
-
 start1 = Start1()
 boot1 = Boot1()
-led1 = Led1()
-led2 = Led2()
+led1, led2 = Led1(), Led2()
 ledRgb1 = LedRgb1()
-dist1, prawyCzujnik, lewyCzujnik, glownyCzujnik = dists_init()
-
-grd1, grd2, grd3, grd4 = grds_init()
-
+prawyCzujnik, glownyCzujnik, lewyCzujnik, dist4 = dists_init()
+lewaZiemia, prawaZiemia, grd3, grd4 = grds_init()
+lewySilnik, prawySilnik= motors_init()
 vBat = VBat()
 
-ledRgb1.brightness = 0.3
-ledRgb1.value = Color.RED
+GRD_TRESHOLD = .7
+DIST_TRESHOLD = .9
+SEARCH_POWER = 0
+ATTACK_POWER = 0
 
-lewySilnik.power = 0
-prawySilnik.power = 0
+def prosto():
+    lewySilnik.power = SEARCH_POWER
+    lewySilnik.forward()
+    prawySilnik.power = SEARCH_POWER
+    prawySilnik.forward()
+    #ledRgb1.value = Color.WHITE
 
+def prostoAtak():
+    lewySilnik.power = ATTACK_POWER
+    lewySilnik.forward()
+    prawySilnik.power = ATTACK_POWER
+    prawySilnik.forward()
+    #ledRgb1.value = Color.RED
+
+def prostoPrawo():
+    lewySilnik.power = ATTACK_POWER
+    lewySilnik.forward()
+    prawySilnik.power = ATTACK_POWER/4
+    prawySilnik.forward()
+    #ledRgb1.value = Color.WHITE
+
+def prostoLewo():
+    lewySilnik.power = ATTACK_POWER/4
+    lewySilnik.forward()
+    prawySilnik.power = ATTACK_POWER
+    prawySilnik.forward()
+    #ledRgb1.value = Color.WHITE
+
+def tyl():
+    lewySilnik.power = SEARCH_POWER
+    lewySilnik.backward()
+    prawySilnik.power = SEARCH_POWER
+    prawySilnik.backward()
+    #ledRgb1.value = Color.YELLOW
+
+def tylLewo():
+    lewySilnik.power = SEARCH_POWER/4
+    lewySilnik.backward()
+    prawySilnik.power = SEARCH_POWER
+    prawySilnik.backward()
+    #ledRgb1.value = Color.YELLOW
+
+def tylPrawo():
+    lewySilnik.power = SEARCH_POWER
+    lewySilnik.backward()
+    prawySilnik.power = SEARCH_POWER/4
+    prawySilnik.backward()
+    #ledRgb1.value = Color.YELLOW
+
+def stop():
+    lewySilnik.stop()
+    prawySilnik.stop()
+    ledRgb1.value = Color.GREEN
+
+def lewo():
+    lewySilnik.power = SEARCH_POWER
+    lewySilnik.backward()
+    prawySilnik.power = SEARCH_POWER
+    prawySilnik.forward()
+    #ledRgb1.value = Color.PINK
+
+def prawo():
+    lewySilnik.power = SEARCH_POWER
+    lewySilnik.forward()
+    prawySilnik.power = SEARCH_POWER
+    prawySilnik.backward()
+    #ledRgb1.value = Color.PINK
+
+def odliczanie():
+    for i in range(10):
+        ledRgb1.value = (250 - i*5, i*5, 0)
+        time.sleep(0.1)
+    for i in range(10):
+        ledRgb1.value = (200 - i*5, 50 + i*5, 0)
+        time.sleep(0.1)
+    for i in range(10):
+        ledRgb1.value = (150 - i*5, 100 + i*5, 0)
+        time.sleep(0.1)
+    for i in range(10):
+        ledRgb1.value = (100 - i*5, 150 + i*5, 0)
+        time.sleep(0.1)
+    for i in range(10):
+        ledRgb1.value = (50 - i*5, 200 + i*5, 0)
+        time.sleep(0.1)
+
+def decide():
+    d1 = prawyCzujnik.value > DIST_TRESHOLD
+    d2 = glownyCzujnik.value > DIST_TRESHOLD
+    d3 = lewyCzujnik.value > DIST_TRESHOLD
+
+    g1 = lewaZiemia.value < GRD_TRESHOLD
+    g2 = prawaZiemia.value < GRD_TRESHOLD
+    
+    print('l ', lewaZiemia.value, g1, 'p ', prawaZiemia.value, g2)
+
+    led1.value = g1
+    led2.value = g2
+
+    if d1 or d2 or d3:
+        # na wprost
+        if d2:
+            prostoAtak()
+        # po lewej
+        elif d3:
+            prostoLewo()
+        # po prawej
+        elif d1:
+            prostoPrawo()
+
+    elif g1 or g2:
+        if g1 and g2:
+            tylPrawo()
+            time.sleep(0.5)
+        elif g1:
+            tylPrawo()
+            time.sleep(0.5)
+        elif g2:
+            tylLewo()
+            time.sleep(0.5)
+    else:
+        prosto()
+
+ledRgb1.value = Color.BLACK
 start1.waitFor()
 
-ledRgb1.value = Color.GREEN
+#countdown()
 
-while True:
-    #print(lewySilnik, prawySilnik, lewyCzujnik.value, prawyCzujnik.value, glownyCzujnik.value)
-    lewySilnik.power = 0
-    prawySilnik.power = 0
-    lewySilnik.forward()
-    prawySilnik.forward()
+while (True):
+    decide()
+    time.sleep(0.02)
 
-    print('GRD1:', grd1, 'GRD2:', grd2, 'GRD3:', grd3, 'GRD4:', grd4)
-
-    if lewyCzujnik.value > 1.3 and glownyCzujnik.value < 1.3:
-        prawySilnik.power = .6
-    elif prawyCzujnik.value > 1.3 and glownyCzujnik.value < 1.3:
-        lewySilnik.power = .6
-
-    if glownyCzujnik.value > 1.3:
-        lewySilnik.power = 1
-        prawySilnik.power = 1
-
-    if boot1.value:
-        lewySilnik.stop()
-        prawySilnik.stop()
-        ledRgb1.value = Color.GREEN
-        break
-    time.sleep(0.1)
